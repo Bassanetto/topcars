f5040f1f3dd648085888aea11024b0255d02bc30
alopes-tenor <83618366+alopes-tenor@users.noreply.github.com>
2022-12-04 14:30:24 -0300

    Tabelas Banco de Dados
    Envio do script das tabelas do Banco de Dados

---
+++ script_tabelas_FBII.sql
@@ -0,0 +1,36 @@
+#create database bytesql;
+use bytesql;
+
+
+CREATE TABLE `veiculo` (
+  `id_veiculo` int NOT NULL AUTO_INCREMENT,
+  `modelo` varchar(50) NOT NULL,
+  `cor` varchar(50) NOT NULL,
+  `ano` varchar(50) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `placa` varchar(50) NOT NULL,
+  `carroceria` varchar(50) NOT NULL,
+  `motor` varchar(45) NOT NULL,
+  `combustivel` varchar(45) NOT NULL,
+  `cambio` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_veiculo`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE locacao(
+id_locacao int not null auto_increment primary key,
+nome_locacao varchar(60) not null,
+cnh_locacao varchar(60) not null,
+cpf_locacao varchar(60) not null,
+data_locacao date not null,
+carro_locacao varchar(45) not null,
+placa_locacao varchar(45) not null);
+
+create table logins(
+id_usuario int not null auto_increment primary key,
+usuario varchar(45) not null,
+senha varchar(45) not null,
+nome varchar(60) not null,
+sobrenome varchar(60) not null,
+email varchar(45) not null,
+cpf varchar(45) not null)
+
